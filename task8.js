6929262ecacebbe19d1696ac4609193315c8c8f9
EngrWaqas <62841930+EngrWaqas@users.noreply.github.com>
2023-04-11 15:44:22 +0500

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,4 +1,11 @@
-Write a function that takes an object and saves each property to
-localStorage using the property name as the key and the property value as
-the value. The function should also retrieve the object from localStorage
-and return it as a new object
+//Write a function that takes an object and saves each property to
+//localStorage using the property name as the key and the property value as
+//the value. The function should also retrieve the object from localStorage
+//and return it as a new object
+
+
+
+
+
+
+// hi there sir i couldn't understand this 8th task i'll assure you i work on it 
